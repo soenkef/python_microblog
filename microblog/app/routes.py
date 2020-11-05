7c77b9af24ac7daa4d305e4cc7278df1aceb8a0d
soenkef <mantu@gmx.de>
2020-11-05 10:23:02 +0100

    Microblog Teil 3 - Loginformular mit Verarbeitung gebaut

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,30 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # Ein paar Testeinträge werden in Dictionarys erstellt
     user = {'username': 'Soenke'}
-    return render_template('index.html', user=user)
-    #return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susam'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST']) #Formular soll get und Post verarbeiten
+def login():
+    form = LoginForm()
+    # Formulareingaben werden automatisch verarbeitet
+    if form.validate_on_submit():
+        flash('Login requested from user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
