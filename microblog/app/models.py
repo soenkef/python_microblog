299e21c327a6d0fa11e6592d3662494489d92e6f
soenkef <mantu@gmx.de>
2020-11-06 09:27:52 +0100

    Microblog - Registrierungsformular und Profilseite mit Avataren erstellt

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 from app import db
@@ -19,6 +20,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -10,7 +10,7 @@ from werkzeug.urls import url_parse
 @login_required
 def index():
     # Ein paar Testeinträge werden in Dictionarys erstellt
-    user = {'username': 'Soenke'}
+    #user = {'username': 'Soenke'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -21,7 +21,7 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST']) #Formular soll get und Post verarbeiten
 def login():
@@ -45,3 +45,27 @@ def login():
 def logout():
     logout_user();
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
