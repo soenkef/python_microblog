76000a165e5272330b0cef1e886de049e136eadc
soenkef <mantu@gmx.de>
2020-11-13 12:55:17 +0100

    Passwort zurücksetzen wird gebaut, mit JWT

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-#from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 from app import routes, models, errors
 
@@ -31,21 +33,20 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-# EMAIL funktioniert noch nicht: es gibt einen KeyError=['MAIL_SERVER'], Kapitel 7
-
-#if not app.debug:
-#    if app.config['MAIL_SERVER']:
-#        auth = None
-#        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-#            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-#        secure = None
-#        if app.config['MAIL_USE_TLS']:
-#            secure = ()
-#        mail_handler = SMTPHandler(
-#            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-#            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-#            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-#            credentials=auth, secure=secure)
-#        mail_handler.setLevel(logging.ERROR)
-#        app.logger.addHandler(mail_handler)
+# EMAIL, Kapitel 7
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
--- microblog/config.py
+++ microblog/config.py
@@ -10,9 +10,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 25
     # Email-Settings
-    #MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    #MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    #ADMINS = ['your-email@example.com']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
