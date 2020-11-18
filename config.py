4f81b7589d7c1fd5e8630f6be26bf394a6d972a6
soenkef <mantu@gmx.de>
2020-11-18 11:08:17 +0100

    .env file angelegt, Email wirft Fehler?

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
-# Sprachunterstützung
-    # LANGUAGES = ['de', 'en', 'es']
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    #SQL-Alchemy und flask-migrate
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 25
+    # Email-Settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    # Sprachunterstützung
+    LANGUAGES = ['de', 'en']
+    # Microsoft Azure-Api-key
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
